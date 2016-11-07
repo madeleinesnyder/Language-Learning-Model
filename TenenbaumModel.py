82413d3d6cd57656ad1929e563b53db662424518
Madeleine Snyder <madeleine.snyder.48@gmail.com>
2016-11-06 19:50:04 -0500

    Added file

---
+++ TenenbaumModel.py
@@ -0,0 +1,7 @@
+# Script to begin the Bayesian inference of what a word refers to.
+
+# Prior heriarchical structure determines the linkage between different object trees.
+# An object tree is, for example, dalmatian --> dog --> animal 
+# An object is defined by its position in the tree and its distance from other objects
+
+# A word's reference space is defined by the size of things that are connected to it in the object tree
